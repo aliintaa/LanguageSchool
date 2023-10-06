d68a07865e162d1acb05f4f7cba667eb2e30f7dd
almil <atkzn.05@mail.ru>
2023-10-06 12:08:57 +0300

    переделала

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Frames.Navigate(new mypage.Page1());
+            //    var path = @"C:\Users\222126\Desktop\Сессия 1\";
+            //    foreach(var item in App.db.Service.ToArray())
+            //    {
+            //        var fullPath = path + item.MainImagePath.Trim();
+            //        var imageByte = File.ReadAllBytes(fullPath);
+            //        item.MainImage = imageByte;
+
+        }
+        //    App.db.SaveChanges();
+        //}
+    }
+}
---
+++ WpfApp1/WpfApp1/mypage/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Componens;
+
+namespace WpfApp1.mypage
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            var services = App.db.Service.ToList();
+            foreach (var service in services)
+            {
+                ServiceWp.Children.Add(new ServaseUserControl(new Image(), service.Title, service.Cost.ToString(), service.Discount.ToString()));
+            }
+        }
+    }
+}
